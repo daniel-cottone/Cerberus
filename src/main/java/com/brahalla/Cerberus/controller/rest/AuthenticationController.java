1edd8913095ad27eee3ad4002a2c14b939812f24
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-10 10:26:48 -0500

    meh

--- src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
+++ src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.brahalla.Cerberus.controller.rest;
 
-import com.brahalla.Cerberus.configuration.TokenAuthenticationService;
+import com.brahalla.Cerberus.configuration.UserAuthentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
   @Autowired
-  TokenAuthenticationService tokenAuthenticationService;
+  AuthenticationManager authenticationManager;
 
   @RequestMapping(method = RequestMethod.POST)
-  public String attemptAuthentication(@RequestBody String username) {
+  public String authenticationRequest(@RequestBody String username) throws AuthenticationException {
+    UserAuthentication userAuthentication = this.authenticationManager.authenticate(
+      new UsernamePasswordAuthenticationToken(
+        username,
+        username
+      )
+    );
     return "hello";
   }
 
