de7a81f10c51d2cd56b076f847e2ba4b85c027b4
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-10 17:00:06 -0500

    lets see if this works

--- src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
+++ src/main/java/com/brahalla/Cerberus/controller/rest/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.brahalla.Cerberus.controller.rest;
 
 import com.brahalla.Cerberus.configuration.TokenAuthenticationService;;
+import com.brahalla.Cerberus.configuration.UserAuthentication;
 import com.brahalla.Cerberus.model.json.AuthenticationRequest;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,16 +30,15 @@ public class AuthenticationController {
   TokenAuthenticationService tokenAuthenticationService;
 
   @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity<?> authenticationRequest(@RequestBody AuthenticationRequest authenticationRequest) throws AuthenticationException {
-    ResponseEntity<?> response = null;
+  public ResponseEntity<?> authenticationRequest(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse response) throws AuthenticationException {
     Authentication authentication = this.authenticationManager.authenticate(
       new UsernamePasswordAuthenticationToken(
         authenticationRequest.getUsername(),
         authenticationRequest.getPassword()
       )
     );
-    response = ResponseEntity.ok(null);
-    return response;
+    this.tokenAuthenticationService.addAuthentication(response, (UserAuthentication) authentication);
+    return ResponseEntity.ok(null);
   }
 
 }
