df71a6df42b70cafbfa2ac1f3d53c1069ebf6f1a
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-10 08:04:59 -0500

    initial commit

---
+++ src/main/java/com/brahalla/Cerberus/configuration/AuthorizationServerConfiguration.java
@@ -0,0 +1,14 @@
+package com.brahalla.Cerberus.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+
+}
