2b409a7f23426c6f6004480b13bfe7b1b5ea0895
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-10 21:15:13 -0500

    derp

--- src/main/java/com/brahalla/Cerberus/configuration/UserDetailsServiceImpl.java
+++ src/main/java/com/brahalla/Cerberus/configuration/UserDetailsServiceImpl.java
@@ -2,24 +2,26 @@ package com.brahalla.Cerberus.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserDetailServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     if (username != "user") {
       throw new UsernameNotFoundException("no user found with " + username);
     }
-    return new UserDetails(
+    return new User(
       "user",
       "password",
       true, true, true, true,
       AuthorityUtils.createAuthorityList("USER")
     );
   }
+
 }
