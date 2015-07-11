b0d148590b8104a882352bf9644e704b9f8daa99
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-10 21:58:57 -0500

    hm

--- src/main/java/com/brahalla/Cerberus/configuration/UserDetailsServiceImpl.java
+++ src/main/java/com/brahalla/Cerberus/configuration/UserDetailsServiceImpl.java
@@ -6,9 +6,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
   @Override
