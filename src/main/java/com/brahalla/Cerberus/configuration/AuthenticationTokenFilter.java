0c235509d25de0eb171b8d3ad792dd71b3dbbb3d
Daniel Cottone <daniel.cottone@gmail.com>
2015-07-11 07:19:18 -0500

    adding debug

--- src/main/java/com/brahalla/Cerberus/configuration/AuthenticationTokenFilter.java
+++ src/main/java/com/brahalla/Cerberus/configuration/AuthenticationTokenFilter.java
@@ -29,9 +29,13 @@ public class AuthenticationTokenFilter extends UsernamePasswordAuthenticationFil
     String authToken = httpRequest.getHeader("X-Auth-Token");
     String username = TokenUtils.getUsernameFromToken(authToken);
 
+    System.out.println("**************\r\n\r\n" + "Username: " + username + "\r\n");
+
     if (username != null) {
+      System.out.println("**************\r\n\r\nGot here!\r\n");
       UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
       if (TokenUtils.validateToken(authToken, userDetails)) {
+        System.out.println("**************\r\n\r\nGot here!\r\n");
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
         SecurityContextHolder.getContext().setAuthentication(authentication);
