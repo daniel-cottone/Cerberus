15a37e4ebc93195d7b1626bf97b06c38ecc512e2
Frank Dai <soulmachine@gmail.com>
2015-11-18 18:18:14 -0800

    Supported token expiration

--- src/main/java/com/brahalla/Cerberus/security/TokenUtils.java
+++ src/main/java/com/brahalla/Cerberus/security/TokenUtils.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class TokenUtils {
 
   @Value("${cerberus.token.secret}")
   private String secret;
+  @Value("${cerberus.token.expiration}")
+  private long expiration;
 
   public String getUsernameFromToken(String token) {
     String username;
@@ -26,15 +30,33 @@ public class TokenUtils {
     return username;
   }
 
+  private Date getExpirationDate(String token) {
+    Date expiration;
+    try {
+      expiration = Jwts.parser()
+              .setSigningKey(secret)
+              .parseClaimsJws(token)
+              .getBody()
+              .getExpiration();
+    } catch (Exception e) {
+      expiration = null;
+    }
+    return expiration;
+  }
+
   public String generateToken(UserDetails userDetails) {
     return Jwts.builder()
-      .setSubject(userDetails.getUsername())
+            .setSubject(userDetails.getUsername())
+            .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
       .signWith(SignatureAlgorithm.HS512, secret)
       .compact();
   }
 
   public boolean validateToken(String token, UserDetails userDetails) {
-    return (getUsernameFromToken(token).equals(userDetails.getUsername()));
+    final String username = getUsernameFromToken(token);
+    final Date expiration = getExpirationDate(token);
+    return username.equals(userDetails.getUsername()) &&
+            expiration.after(new Date(System.currentTimeMillis()));
   }
 
 }
